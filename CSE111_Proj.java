bac1853abc7bb8edd2cf81170d8b7b699bc198cd
Khai-Yuan Liew <kliew@ucmerced.edu>
2018-12-02 18:56:40 -0800

    Add files via upload

--- CSE111_Proj.java
+++ CSE111_Proj.java
@@ -7,12 +7,12 @@ public class CSE111_Proj {
 	
 	public static void insertTuple(Connection conn) throws SQLException{
 		 PreparedStatement stat = null;
-
+		 
 		 String table = " ";
 	     Scanner in = new Scanner (System.in);
-
-		 int input = 0;
-
+		 
+		 int input = 0;	 
+		 
 		 //1 - console
 		 //2 - customer
 		 //3 - developer
@@ -278,25 +278,226 @@ public class CSE111_Proj {
 					
 					if(table == "console"){
 						
+						System.out.println("Enter 1 if you want to delete a console from the database");
+						System.out.println("Enter 2 if you want to delete a console in a region from the database");
+						System.out.println("Enter any key to terminate the deletion.");
+						int prompt = in.nextInt();
+						
+						if(prompt == 1){
+							
+							System.out.println("Enter the console key: ");
+							int conkey = in.nextInt();
+							
+							SQL = "DELETE FROM " + table + " WHERE con_consolekey = ?";
+							
+							stat = conn.prepareStatement(SQL);
+												
+							stat.setInt(1, conkey);
+							
+							
+						}else if (prompt == 2){
+							
+							System.out.println("Enter the console key: ");
+							int conkey = in.nextInt();
+							
+							System.out.println("Enter the region key: ");							
+							int regionkey = in.nextInt();
+							
+							SQL = "DELETE FROM " + table + " WHERE con_consolekey = ? AND con_regionkey = ?";
+							
+							stat = conn.prepareStatement(SQL);
+												
+							stat.setInt(1, conkey);
+							stat.setInt(2, regionkey);
+							
+						}else{
+							return;
+						}
+						
 					}else if(table == "customer"){
+						//1. Delete by c_key - Doesn't exist anymore
+						
+						System.out.println("Enter the customer key: ");
+						int custkey = in.nextInt();
+						
+						SQL = "DELETE FROM " + table + " WHERE c_custkey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setInt(1, custkey);
 						
 					}else if(table == "developer"){
+						//1. Delete by d_key - Doesn't exist anymore
+						//2. Delete by d_publisher - Doesn't exist anymore
+						
+						System.out.println("Enter 1 if you want to delete a specified developer from the DB");
+						System.out.println("Enter 2 if you want to delete a specified publisher from the DB");
+						System.out.println("Enter any key to terminate the deletion.");
+						int prompt = in.nextInt();
+						
+						if(prompt == 1){
+							
+						System.out.println("Enter the developer key: ");
+						int devkey = in.nextInt();
+						
+						SQL = "DELETE FROM " + table + " WHERE d_devkey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setInt(1, devkey);
+						
+							
+						}else if(prompt == 2){
+							
+						in.nextLine();
+						
+						System.out.println("Enter the publisher name: ");
+						String pubname = in.nextLine();
+						
+						SQL = "DELETE FROM " + table + " WHERE d_publisher = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setString(1, pubname);
+						
+							
+						}else{
+							return;
+						}
+						
 						
 					}else if(table == "game"){
+						//Save for last.
+						//1. Delete by gamekey - Doesn't exist anymore/Not selling well.
+						//2. Delete by devkey - Doesn't exist anymore/Liquidated.
+						//3. Delete by regionkey with either dev or gamekey - Not selling well.
+						//4. Delete old games (release date)
+						//5. Delete by console key based on gamekey and/or region
+						System.out.println("Enter 1 if you want to delete a specified game");
+						System.out.println("Enter 2 if you want to delete all games from a developer");
+						System.out.println("Enter 3 if you want to delete a specified game from a region");
+						System.out.println("Enter 4 if you want to delete records that are released eariler than the specified date");
+						System.out.println("Enter 5 if you want to delete a game in a specified console platform in a region");
+						System.out.println("Enter any key to terminate the deletion.");
+						int prompt = in.nextInt();
+						
+						if(prompt == 1){
+							
+						System.out.println("Enter the game key: ");
+						int gamekey = in.nextInt();
+							
+						SQL = "DELETE FROM " + table + " WHERE g_gamekey = ?";
+							
+						stat = conn.prepareStatement(SQL);
+							
+						stat.setInt(1, gamekey);
+							
+						}else if(prompt == 2){
+							
+						System.out.println("Enter the developer key: ");
+						int devkey = in.nextInt();
+								
+						SQL = "DELETE FROM " + table + " WHERE g_devkey = ?";
+								
+						stat = conn.prepareStatement(SQL);
+								
+						stat.setInt(1, devkey);
+							
+						}else if(prompt == 3){
+							
+						System.out.println("Enter the game key: ");
+						int gamekey = in.nextInt();
+						
+						System.out.println("Enter the region key: ");
+						int regionkey = in.nextInt();
+								
+						SQL = "DELETE FROM " + table + " WHERE g_gamekey = ? AND g_regionkey = ?";
+								
+						stat = conn.prepareStatement(SQL);
+								
+						stat.setInt(1, gamekey);
+						stat.setInt(2, regionkey);
+							
+						}else if(prompt == 4){
+							
+						System.out.println("Enter the date: ");
+					    String date = in.next();							
+							
+						SQL = "DELETE FROM " + table + " WHERE g_releasedate <= ?";
+							
+						stat = conn.prepareStatement(SQL);
+									
+						stat.setString(1, date);
+							
+						}else if(prompt == 5){
+							
+						System.out.println("Enter the game key: ");
+						int gamekey = in.nextInt();
+							
+						System.out.println("Enter the region key: ");
+						int regionkey = in.nextInt();
+						
+						System.out.println("Enter the console key: ");
+						int conkey = in.nextInt();
+									
+						SQL = "DELETE FROM " + table + " WHERE g_gamekey = ? AND g_regionkey = ? AND g_consolekey = ?";
+									
+						stat = conn.prepareStatement(SQL);
+									
+						stat.setInt(1, gamekey);
+						stat.setInt(2, regionkey);
+						stat.setInt(3, conkey);
+							
+						}else{
+							
+						}
+						
 						
 					}else if(table == "inventory"){
+						//1. Delete custkey due to canceled/fulfilled order.
+						System.out.println("Enter the customer key: ");
+						int custkey = in.nextInt();
+						
+						SQL = "DELETE FROM " + table + " WHERE i_custkey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setInt(1, custkey);
 						
 					}else if(table == "region"){
+						//1. Delete by regionkey
+						System.out.println("Enter the region key: ");
+						int regionkey = in.nextInt();
+						
+						SQL = "DELETE FROM " + table + " WHERE r_regionkey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setInt(1, regionkey);
 						
 					}else if(table == "sales"){
+						//1. Delete sales less than a set amount
+						System.out.println("Enter amount of sales: ");
+						int sales = in.nextInt();
 						
+						SQL = "DELETE FROM " + table + " WHERE s_sales < ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setInt(1, sales);
 					}
+	
+					int del = stat.executeUpdate();
+					
+					System.out.println(del + " Record(s) successfully deleted");
+					
+					conn.commit();
 					
 				}	
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("");
-			System.out.println("Insertion failed. Rollbacking...");
+			System.out.println("Delete failed. Rollbacking...");
 			conn.rollback();
 		}		
 	}
@@ -346,21 +547,292 @@ public class CSE111_Proj {
 					 
 					String SQL = " ";
 					
+					//Should not be changing up keys.
 					if(table == "console"){
+						//1. Price Rise/Reduction
+						
+						System.out.println("Enter the console key you want to update: ");
+						int conkey = in.nextInt();
+						
+						System.out.println("Enter the region key you want to update: ");
+						int regionkey = in.nextInt();
+						
+						System.out.println("Enter the price: ");
+						double price = in.nextDouble();
+						
+						
+						SQL = "UPDATE " + table + " SET con_price = ? " + " WHERE con_consolekey = ? AND con_regionkey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setDouble(1, price);
+						stat.setInt(2, conkey);
+						stat.setInt(3, regionkey);
+						
 						
 					}else if(table == "customer"){
+						//1. c_name - Person under new name.
+						//2. c_phone - Changed Phone num.
+						//3. c_address - Change address/Moved
+						//4. c_credcardnum - Changed Credit Cards.
+						
+						System.out.println("Enter 1 if you want to update a customer name");
+						System.out.println("Enter 2 if you want to update a customer phone number");
+						System.out.println("Enter 3 if you want to update a customer address");
+						System.out.println("Enter 4 if you want to update a customer credcardnum");						
+						System.out.println("Enter any key to terminate the update.");
+						int prompt = in.nextInt();
+						
+						if(prompt == 1){
+							
+						System.out.println("Enter the customer key you want to update: ");
+						int custkey = in.nextInt();
+						
+						in.nextLine();
+						
+						System.out.println("Enter the new name: ");
+						
+						String name = in.nextLine();
+							
+						SQL = "UPDATE " + table + " SET c_name = ? " + " WHERE c_custkey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setString(1, name);
+						stat.setInt(2, custkey);
+							
+						}else if(prompt == 2){
+							
+						System.out.println("Enter the customer key you want to update: ");
+						int custkey = in.nextInt();
+							
+						System.out.println("Enter the new phone number: ");
+							
+						String phonenum = in.next();
+						
+						SQL = "UPDATE " + table + " SET c_phone = ? " + " WHERE c_custkey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setString(1, phonenum);
+						stat.setInt(2, custkey);
+							
+						}else if(prompt == 3){
+							
+						System.out.println("Enter the customer key you want to update: ");
+						int custkey = in.nextInt();
+						
+						in.nextLine();
+								
+						System.out.println("Enter the new address: ");
+								
+						String address = in.nextLine();
+							
+						SQL = "UPDATE " + table + " SET c_address = ? " + " WHERE c_custkey = ?";
+							
+						stat = conn.prepareStatement(SQL);
+							
+						stat.setString(1, address);
+						stat.setInt(2, custkey);
+							
+						}else if(prompt == 4){
+							
+						System.out.println("Enter the customer key you want to update: ");
+						int custkey = in.nextInt();
+									
+						System.out.println("Enter the new credit card number: ");
+									
+						String credcardnum = in.next();
+								
+						SQL = "UPDATE " + table + " SET c_credcardnum = ? " + " WHERE c_custkey = ?";
+								
+						stat = conn.prepareStatement(SQL);
+								
+						stat.setString(1, credcardnum);
+						stat.setInt(2, custkey);
+							
+						}else{
+							return;
+						}
+						
 						
 					}else if(table == "developer"){
+						//1. d_name - Renamed
+						//2. d_publisher - Changed Ownership or Renamed.
+						//3. d_address - Changed address.
+						
+						System.out.println("Enter 1 if you want to update a developer name");
+						System.out.println("Enter 2 if you want to update a publisher name");
+						System.out.println("Enter 3 if you want to update an office address");						
+						System.out.println("Enter any key to terminate the update.");
+						int prompt = in.nextInt();
+						
+						if(prompt == 1){
+							
+						System.out.println("Enter the developer key you want to update: ");
+						int devkey = in.nextInt();
+						
+						in.nextLine();
+						
+						System.out.println("Enter the new developer name: ");
+						
+						String devname = in.nextLine();
+						
+						SQL = "UPDATE " + table + " SET d_devname = ? " + " WHERE d_devkey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+								
+						stat.setString(1, devname);
+						stat.setInt(2, devkey);
+							
+						}else if(prompt == 2){
+						
+						in.nextLine();
+						
+						System.out.println("Enter the publisher name you want to update: ");
+						
+						String pubname_in = in.nextLine();
+						
+						System.out.println("Enter the new publisher name: ");
+						
+						String pubname_out = in.nextLine();	
+						
+						SQL = "UPDATE " + table + " SET d_publisher = ? " + " WHERE d_publisher = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+								
+						stat.setString(1, pubname_out);
+						stat.setString(2, pubname_in);
+						
+							
+						}else if(prompt == 3){
+							
+						System.out.println("Enter the developer key you want to update: ");
+						int devkey = in.nextInt();
+							
+						in.nextLine();
+							
+						System.out.println("Enter the new developer name: ");
+							
+						String address = in.nextLine();
+							
+						SQL = "UPDATE " + table + " SET d_officeaddress = ? " + " WHERE d_devkey = ?";
+							
+						stat = conn.prepareStatement(SQL);
+									
+						stat.setString(1, address);
+						stat.setInt(2, devkey);
+							
+						}else{
+							return;
+						}
+						
 						
 					}else if(table == "game"){
+						//1. g_price with specifed regionkey, consolekey, gamekey. Price rise/reduction
+						
+						System.out.println("Enter the console key you want to update to: ");
+						int conkey = in.nextInt();
+						
+						System.out.println("Enter the region key you want to update to: ");
+						int regionkey = in.nextInt();
+						
+						System.out.println("Enter the game key you want to update to: ");
+						int gamekey = in.nextInt();
+						
+						System.out.println("Enter the price: ");
+						double price = in.nextDouble();
+						
+						SQL = "UPDATE " + table + " SET g_price = ? " + " WHERE g_consolekey = ? AND g_regionkey = ? AND g_gamekey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setDouble(1, price);
+						stat.setInt(2, conkey);
+						stat.setInt(3, regionkey);
+						stat.setInt(4, gamekey);
+						
 						
 					}else if(table == "inventory"){
+						//1. i_gamekey with i_consolekey and i_price changing.
+						
+						System.out.println("Enter the customer key you want to update: ");
+						int custkey = in.nextInt();
+						
+						System.out.println("Enter the game key you want to update: ");
+						int gamekey_out = in.nextInt();
+						
+						System.out.println("Enter the console key you want to update: ");
+						int conkey_out = in.nextInt();
+						
+						System.out.println("Enter the new game key: ");
+						int gamekey_in = in.nextInt();
+						
+						System.out.println("Enter the new console key: ");
+						int conkey_in = in.nextInt();
+						
+						System.out.println("Enter the price: ");
+						double price = in.nextDouble();
+						
+						SQL = "UPDATE " + table + " SET i_gamekey = ?, i_consolekey = ?, i_price = ? " + " WHERE i_custkey = ? AND i_gamekey = ? AND i_consolekey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setInt(1, gamekey_in);
+						stat.setInt(2, conkey_in);
+						stat.setDouble(3, price);
+						stat.setInt(4, custkey);
+						stat.setInt(5, gamekey_out);
+						stat.setInt(6, conkey_out);
+						
 						
 					}else if(table == "region"){
+						//1. Change region name? Why do that? (Might remove later.)
+						
+						System.out.println("Enter the region key you want to update to: ");
+						int regionkey = in.nextInt();
+						
+						System.out.println("Enter the new region name");
+						String region_name = in.next();
+						
+						SQL = "UPDATE " + table + " SET r_regionname = ? " + " WHERE r_regionkey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setString(1, region_name);
+						stat.setInt(2, regionkey);
 						
 					}else if(table == "sales"){
+						//1. Change sales to reflect current sales.
 						
+						System.out.println("Enter the game key you want to update to: ");
+						int gamekey = in.nextInt();
+						
+						System.out.println("Enter the console key you want to update to: ");
+						int conkey = in.nextInt();
+						
+						System.out.println("Enter the region key you want to update to: ");
+						int regionkey = in.nextInt();
+						
+						System.out.println("Enter the number of sales: ");
+						int sales = in.nextInt();
+						
+						SQL = "UPDATE " + table + " SET s_sales = ? " + " WHERE s_gamekey = ? AND s_consolekey = ? AND s_regionkey = ?";
+						
+						stat = conn.prepareStatement(SQL);
+						
+						stat.setInt(1, sales);
+						stat.setInt(2, gamekey);
+						stat.setInt(3, conkey);
+						stat.setInt(4, regionkey);						
 					}
+					
+					int up = stat.executeUpdate();
+					
+					System.out.println(up + " Record(s) successfully updated");
+					
+					conn.commit();
 					
 				}
 		} catch (SQLException e) {
@@ -370,7 +842,7 @@ public class CSE111_Proj {
 			conn.rollback();
 		}		
 	}
-
+	
     public static void select(Connection conn) throws SQLException{
         PreparedStatement stat = null;
 
@@ -582,11 +1054,9 @@ public class CSE111_Proj {
             System.out.println("Deletion failed. Rollbacking...");
             conn.rollback();
         }
-        return;
-    }
-
-
-
+        	return;
+    }	
+	
 
 	public static void main(String[] args) throws ClassNotFoundException{
 	      Class.forName("org.sqlite.JDBC");
@@ -596,7 +1066,7 @@ public class CSE111_Proj {
 	      
 		try {
 			int input = 0;
-			connection = DriverManager.getConnection("jdbc:sqlite:Project.db");
+			connection = DriverManager.getConnection("jdbc:sqlite:CSE111_Project.db");
 			System.out.println("GameShop Database Version 1 (11/28/2018). Manager Use Only.\n");
 			
 			while(input != -1){				
@@ -621,7 +1091,8 @@ public class CSE111_Proj {
 					//SELECT
 					select(connection);
 					//Put a method to implement select statement.
-
+					
+					System.out.println("Functionality Coming Soon!");
 				}
 			}
 			
